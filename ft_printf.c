05c5979169df8a3d246e860d1cdb2cbb67f6d02f
Nicholas Saraiva <nicholas.saraivadev@gmail.com>
2025-06-18 17:24:55 +0100

    Adding configurations on flag 'c'

--- ft_printf.c
+++ ft_printf.c
@@ -1,53 +1,105 @@
 #include "ft_printf.h"
+#include <stdint.h>
 #include <stdarg.h>
 #include <unistd.h>
 #include <stdio.h>
 
-char	fill_flags(int *flag, char *c)
+int	is_flag(char my_char)
 {
-	char	*my_char;
+	const char	flags[] = "-+0#. ";
+	int		i;
+
+	i = 0;
+	while (flags[i])
+	{
+		if (my_char == flags[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	fill_flags(int *flag, const char *c)
+{
+	const char	*my_char;
 
 	my_char = c;
-
-	while (*my_char)
+	while (is_flag(*my_char))
 	{
 		if (*my_char == '+')
-			flag |= FLAG_PLUS;
-
-		if (*my_char == ' ' && !(flag & FLAG_PLUS))
-			flag |= FLAG_SPACE;
+			*flag |= FLAG_PLUS;
+		if (*my_char == ' ' && !(*flag & FLAG_PLUS))
+			*flag |= FLAG_SPACE;
 		if (*my_char == '-')
 		{
-			flag |= FLAG_MINUS;
-			while (*my_char && (my_char >= '0' && my_char <= '9'))
+			*flag |= FLAG_MINUS;
+			my_char++;
+			while (*my_char && (*my_char >= '0' && *my_char <= '9'))
 				my_char++;
-			if (*my_char == '#' || *my_char == '+' || *my_char == ' ' ||*my_char == '-')
-				return (NULL);
-			continue ;
+			if (*my_char == '#' || *my_char == '+' || *my_char == ' ' || *my_char == '-')
+				return (0);
 		}
-		if (*my_char == '0' && !(flag & FLAG_MINUS))
-			flag |= FLAG_ZERO;
-			while (*my_char && (my_char >= '0' && my_char <= '9'))
+		if (*my_char == '0' && !(*flag & FLAG_MINUS))
+		{
+
+			*flag |= FLAG_ZERO;
+			while (*my_char && (*my_char >= '0' && *my_char <= '9'))
 				my_char++;
 			if (*my_char == '#' || *my_char == '+' || *my_char == ' ')
-				return (NULL);
-			continue ;
+				return (0);
+		}
 		if (*my_char == '.')
-			flag |= FLAG_DOT;
-			while (*my_char && (my_char >= '0' && my_char <= '9'))
+		{
+			*flag |= FLAG_DOT;
+			while (*my_char && (*my_char >= '0' && *my_char <= '9'))
 				my_char++;
 			if (*my_char == '#' || *my_char == '+' || *my_char == ' ' || *my_char == '.')
-				return (NULL);
-			continue ;
+				return (0);
+		}
 		if (*my_char == '#')
-			flag |= FLAG_HASH;
+			*flag |= FLAG_HASH;
+	}
+	return (1);
+}
+
+void	char_flag(const char *c)
+{
+	int value = 0;
+	const char	*d;
+
+	d = c;
+	ft_putchar(*c);
+	d++;
+	while (*d != 'c')
+	{
+		value += value*10 + (*d - '0');
+		d++;
+	}
+	while (--value)
+		ft_putchar(' ');
+}
+
+void	check_condition(int flag, const char *c, va_list args)
+{
+	char	my_arg;
+
+	my_arg = 0;
+	if (*c == 'c' || is_flag(*c))
+	{
+		my_arg	= (char)va_arg(args, int);
+		if (flag && flag ^ FLAG_MINUS)
+			return ;
+		if (flag & FLAG_MINUS)
+			char_flag(c);
+		else
+			ft_putchar(my_arg);
 	}
 }
 
 int	ft_printf(const char *fstring, ...)
 {
 	va_list	args;
-	char	*c;
+	const char	*c;
 	int		flag;
 
 	va_start(args, fstring);
@@ -57,26 +109,23 @@ int	ft_printf(const char *fstring, ...)
 	{
 		if(*c != '%')
 		{
-			putchar(*c);
+			ft_putchar(*c);
 			c++;
 			continue ;
 		}
-		p++;
-		fill_flags(&flag, c, args);
-		if (!flag)
+		c++;
+		if (!fill_flags(&flag, c))
 		{
-			puthcar('%')
+			ft_putchar('%');
 			continue ;
 		}
+		check_condition(flag, c++, args);
 	}
-	va_end(ap);
+	va_end(args);
+	return (1);
 }
 
 int	main()
 {
-	char	ola[] = "BUENO :)";
-	int		number	= 3;
-
-	minprintf("HOJE stoy %d dias %s\n", number, ola);
-	printf("HOJE estoy %d dias %s", number, ola);
+	ft_printf("aaaaaaaaaaaaaaaaa %-10ci \\_O,o_/)\n", 'o');
 }
--- ft_putchar.c
+++ ft_putchar.c
@@ -1,25 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_putchar.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: nsaraiva <nsaraiva@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/13 04:07:16 by nsaraiva          #+#    #+#             */
-/*   Updated: 2025/06/18 16:24:26 by nsaraiva         ###   ########.fr       */
+/*   Created: 2025/04/16 14:55:20 by nsaraiva          #+#    #+#             */
+/*   Updated: 2025/06/18 16:23:57 by nsaraiva         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "ft_printf.h"
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-# include <unistd.h>
-
-# define FLAG_MINUS 1 << 0 // 1
-# define FLAG_ZERO 1 << 1 // 2
-# define FLAG_DOT 1 << 2 // 4
-# define FLAG_HASH 1 << 3 // 8
-# define FLAG_SPACE 1 << 4 // 16
-# define FLAG_PLUS 1 << 5 // 32
-
-void	ft_putchar(char c);
-#endif
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
